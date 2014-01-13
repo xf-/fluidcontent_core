fb41fb09c87c65810a85101892ba065b07b04ede
Claus Due <claus@namelesscoder.net>
2014-01-13 23:39:00 +0100

    [TASK] Remove redundant TS inclusion

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,7 +15,7 @@ $GLOBALS['TYPO3_CONF_VARS']['FluidTYPO3.FluidcontentCore']['types'] = array(
 
 \FluidTYPO3\Flux\Core::registerConfigurationProvider('FluidTYPO3\FluidcontentCore\Provider\ContentProvider');
 
-\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:fluidcontent_core/Configuration/TypoScript');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'constants', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fluidcontent_core/Configuration/TypoScript/constants.txt">');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fluidcontent_core/Configuration/TypoScript/setup.txt">');
 
 // Prepare a global variants registration array indexed by CType value.

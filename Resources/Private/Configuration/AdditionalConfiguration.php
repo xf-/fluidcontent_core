5a08dae19eba33ed7a239afbaf530b359312e82e
Claus Due <claus@namelesscoder.net>
2014-12-03 02:00:27 +0100

    [TASK] Restore AdditionalConfiguration.php template as private resource

---
+++ Resources/Private/Configuration/AdditionalConfiguration.php
@@ -0,0 +1,3 @@
+<?php
+$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'] = array('fluidcontentcore/Configuration/TypoScript/');
+$GLOBALS['TYPO3_CONF_VARS']['FE']['activateContentAdapter'] = 0;

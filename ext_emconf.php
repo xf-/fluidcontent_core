49e7db6ad63dc9514c81d735b49618b3b53ccab4
Claus Due <claus@namelesscoder.net>
2015-03-18 16:22:07 +0100

    [TASK] Set correct Flux dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'typo3' => '6.2.0-6.2.99',
 			'cms' => '',
-			'flux' => '7.1.0-7.1.99',
+			'flux' => '7.2.0-7.2.99',
 			'vhs' => '2.1.0-2.3.99',
 		),
 		'conflicts' => array(
